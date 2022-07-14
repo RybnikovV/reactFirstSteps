f408beb455340a99c3d51f549ee649232ea9e9ae
Rybnikov.VV <va6d6@mail.ru>
2022-07-14 19:49:21 +0500

    добавлен функционал вернуться назад на странице - notFound

--- src/pages/not-found/NotFound.js
+++ src/pages/not-found/NotFound.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
 import { Responses404 } from '@consta/uikit/Responses404';
-import { Theme, presetGpnDark, presetGpnDefault, ThemePreset } from '@consta/uikit/Theme';
-
+import { Theme, presetGpnDark } from '@consta/uikit/Theme';
+import MyButton from '../../components/button/btn';
 
 function NotFound() {
+  const navigate = useNavigate();
+
   return (
     <Theme preset={presetGpnDark}>
-      <Responses404 />
+      <Responses404
+        actions={<MyButton onClick={() => navigate(-1)}>go back</MyButton>}
+      />
     </Theme>
   );
 };
